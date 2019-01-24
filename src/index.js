71d84649f1b340f9e32943c4079c4336d91c30d1
Eric Allen <eallen@doterra.com>
2019-01-24 11:12:12 -0700

    Comments on what I did and why

--- src/index.js
+++ src/index.js
@@ -12,19 +12,31 @@ function Square(props) {
 }
 
 class Board extends React.Component {
+    //This is the click handler passed to the child, Square,
+    //when fired in the child its using a prop passed from the
+    //parent, Game, to set the cell number
+    //Personally this isn't the most eloquent way to pass data
+    //from child to parent but seems to be the react way.
     handleClick(i) {
         this.props.onSetCellNumber(i);
     }
 
+    //Common render function for Squares allows reuse to make lots of squares
     renderSquare(i) {
         return (
             <Square
+                //props passed to children
+                //value is just a number
                 value={this.props.squares[calcCell(i, this.props.boardNumber)]}
+                //onClick is passing a function that can be called by its children
                 onClick={() => this.handleClick(calcCell(i, this.props.boardNumber))}
             />
         );
     }
 
+    //render method part of React.Component its the only method that's required to implement
+    //render should be pure, as in it doesn't modify state and always returns the same results
+    //Here it's supplying html as well as passing props onto its children
     render() {
         return (
             <div>
@@ -50,6 +62,10 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    //here is a constructor
+    //super should always be called
+    //I am setting up a set of attributes in the state that can be used
+    //I feel these are like private class variables
     constructor(props) {
         super(props);
         this.state = {
@@ -64,12 +80,15 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        console.log(i + " onClick")
+        console.log(i + " onClick");
+        //this here would allow for replaying of old moves, which I didn't implement
+        //we are copying the array instead of modifying it.
         const squares = this.state.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+        //I am setting the state variables here
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
@@ -87,6 +106,8 @@ class Game extends React.Component {
         );
     }
 
+    //Again the render method from the React.Component
+    //Its rendering and re rendering on changes the html
     render() {
         const winner = calculateWinner(this.state.squares);
         let status;
@@ -124,7 +145,7 @@ function calcCell(cellNumber, boardNumber){
 }
 
 function calculateWinner(squares) {
-    const lines = [
+    const lines = [ //This only wins for the top board or the first column
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
